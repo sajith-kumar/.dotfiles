c5a170c3c2c09afdca42536f9ff5cf4e314c1ecf
Sajith <31896824+sajith-kumar@users.noreply.github.com>
2025-03-15 22:41:34 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt install vim -y
 # Get .vimrc file form github
 #git clone https://github.com/sajith-kumar/dotfiles.git
 # download pluging manager
-cp dotfiles/vimrc ~/.vimrc
+cp ~/dotfiles/vimrc ~/.vimrc
 #Installl plugin manager
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.    vim
 # open vim and execute :PlugInstall command
