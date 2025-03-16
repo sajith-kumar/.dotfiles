f02cfd4c726a38a2cecc377b999464d0a114740f
Sajith <31896824+sajith-kumar@users.noreply.github.com>
2025-03-15 22:58:04 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 sudo apt install vim -y
 
 #Installl plugin manager
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # Get .vimrc file form github
 #git clone https://github.com/sajith-kumar/dotfiles.git
 # download pluging manager
